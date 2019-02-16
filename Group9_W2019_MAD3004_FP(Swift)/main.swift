7663a69823aa31b85019b39c6289e05d52814976
MacStudent <macstudent@126-iMac39.local>
2019-02-15 19:55:33 -0500

    Initial Commit

---
+++ Group9_W2019_MAD3004_FP(Swift)/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Group9_W2019_MAD3004_FP(Swift)
+//
+//  Created by MacStudent on 2019-02-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
