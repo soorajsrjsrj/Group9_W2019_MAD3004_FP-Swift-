9117fbdcd0c73cb5bb1856fff9039d62b0fb472d
MacStudent <macstudent@126-iMac39.local>
2019-02-15 21:23:08 -0500

    initial classes

---
+++ Group9_W2019_MAD3004_FP(Swift)/Customer.swift
@@ -0,0 +1,33 @@
+//
+//  Customer.swift
+//  Group9_W2019_MAD3004_FP(Swift)
+//
+//  Created by MacStudent on 2019-02-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Customer{
+    var customerName:String
+    var address:String
+    var email:String
+    var creditCardInfo:String
+    var shippingInfo:String
+    init(){
+        self.customerName=String()
+        self.address=String()
+        self.email=String()
+        self.creditCardInfo=String()
+        self.shippingInfo=String()
+    }
+    func register(){
+        
+    }
+    func login(){
+        
+    }
+    func updateProfile(){
+        
+    }
+
+}
